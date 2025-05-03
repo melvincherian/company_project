fc18e4af5f2510e6169c7c5ef7c457806d8c33db
Vishnu P <pvishnukakkodi@gmail.com>
2025-05-04 00:56:01 +0530

    Poster Navigation solved

--- lib/controller/poster_controller.dart
+++ lib/controller/poster_controller.dart
@@ -1,10 +1,10 @@
-import 'package:company_project/models/poster_model.dart';
+import 'package:company_project/models/category_modell.dart';
 import 'package:company_project/services/api/poster_service.dart';
 
 class PosterController {
   final PosterService _service=PosterService();
 
-  Future<List<TemplateModel>>loadPosters()async{
+  Future<List<CategoryModel>>loadPosters()async{
     try{
       return await _service.fetchTemplates();
     }catch(e){
